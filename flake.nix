f814f67f85f2713babed2ac2f13b568ed1d4b0ee
Mr. Fluffy <git@sixearsm>
2022-10-26 15:23:19 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "More classes & utilities for working with numbers";
+
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    build-utils.url = "github:sixears/flake-build-utils/r0.0.0.0";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, build-utils }:
+    build-utils.lib.hOutputs self nixpkgs "number" { };
+}
